3d024cf18f71bf576ddef7c1837b86515c83f604
Dan Nawara <dan@gullivar.(none)>
2010-02-15 19:18:30 -0600

    fixing calendar sorting

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,9 +3,8 @@ class EventsController < ApplicationController
   def index
     @service = GCal4Ruby::Service.new
     @service.authenticate("socialbiteevents@gmail.com", "elsol100")
-    @calendars = @service.calendars
     @cal = GCal4Ruby::Calendar.find(@service, "socialbiteevents@gmail.com", {:scope => :first})
-    @events = GCal4Ruby::Event.find(@cal, "",{ :range => {:start => Time.now, :end => 30.days.from_now}}).reverse
+    @events = @cal.events().sort_by {|event| event.start}
   end
 
 end
