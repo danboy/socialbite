6894a1f6b10f84f1b3253c9f4d8b5e6619254f4f
Dan Nawara <dan@gullivar.(none)>
2010-02-15 19:33:28 -0600

    fixing calendar sorting removing old entries

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,7 @@ class EventsController < ApplicationController
     @service = GCal4Ruby::Service.new
     @service.authenticate("socialbiteevents@gmail.com", "elsol100")
     @cal = GCal4Ruby::Calendar.find(@service, "socialbiteevents@gmail.com", {:scope => :first})
-    @events = @cal.events().sort_by {|event| event.start}
+    @events = GCal4Ruby::Event.find(@cal, "",{ :sort_order => :ascending,:range => {:start => Time.now, :end => 30.days.from_now}}).sort_by {|event| event.start}
   end
 
 end
